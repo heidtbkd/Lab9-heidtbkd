f1617008219a0d62b9fc7617e6026ac9b086d0e3
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-05-02 15:06:04 -0400

    Broken pipe?

--- ChatBot-Team08/features/step_definitions/cli_steps.rb
+++ ChatBot-Team08/features/step_definitions/cli_steps.rb
@@ -4,9 +4,9 @@ end
 Given /^I see "([^'']*)"$/ do |arg1|
 	run_interactive(unescape("ruby chatbot.rb"))
 end
+When /^I type "([^'']*)" and press Enter$/ do |input|
+	type(input)
+end
 Then /^I should see "([^""]*)"$/ do |text|
 	assert_partial_output(text)
-end
-When /^I type "([^'']*)" and press Enter$/ do |arg1|
-	pending # express the regexp above with the code you wish you had
 end
