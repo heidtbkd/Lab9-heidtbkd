7ce38cc214acd83db3e6c5b5f89c5041fa6a5ead
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-05-02 15:17:22 -0400

    Fixed 2nd scenario but broke the first ...?

--- ChatBot-Team08/features/step_definitions/cli_steps.rb
+++ ChatBot-Team08/features/step_definitions/cli_steps.rb
@@ -1,12 +1,17 @@
+require 'aruba/api'
+require 'aruba/hooks'
+
+World(Aruba::Api)
+
 Given /^the application is running$/ do
 	run_interactive(unescape("ruby chatbot.rb"))
 end
 Given /^I see "([^'']*)"$/ do |arg1|
 	run_interactive(unescape("ruby chatbot.rb"))
 end
-When /^I type "([^'']*)" and press Enter$/ do |input|
-	type(input)
-end
 Then /^I should see "([^""]*)"$/ do |text|
 	assert_partial_output(text)
+end
+When /^I type "([^'']*)" and press Enter$/ do |input|
+	type(input)
 end
