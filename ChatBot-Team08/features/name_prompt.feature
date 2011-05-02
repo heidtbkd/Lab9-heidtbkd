02057b2176f68b30cdd4d7f4c666b6c4e5563fa2
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-05-02 14:58:10 -0400

    This contains fix for the Given case, and that test has passedworking on the When case

--- ChatBot-Team08/features/name_prompt.feature
+++ ChatBot-Team08/features/name_prompt.feature
@@ -6,7 +6,7 @@ Feature: Prompt a user for their name
 Scenario: Verify the prompt shows up
 	Given the application is running
 	Then I should see "What is your name?"
-	
+		
 Scenario: Prompt the user for name
 	Given the application is running
 	And I see "What is your name?"
--- ChatBot-Team08/features/step_definitions/cli_steps.rb
+++ ChatBot-Team08/features/step_definitions/cli_steps.rb
@@ -1,6 +1,12 @@
 Given /^the application is running$/ do
 	run_interactive(unescape("ruby chatbot.rb"))
 end
+Given /^I see "([^'']*)"$/ do |arg1|
+	run_interactive(unescape("ruby chatbot.rb"))
+end
 Then /^I should see "([^""]*)"$/ do |text|
 	assert_partial_output(text)
+end
+When /^I type "([^'']*)" and press Enter$/ do |arg1|
+	pending # express the regexp above with the code you wish you had
 end
